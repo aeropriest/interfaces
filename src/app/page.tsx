14706bd04924e6eafa88b2f89f08e18704a250c9
aeropriest <ashok.jaiswal@gmail.com>
2024-08-09 07:10:06 +0800

    * added variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-// maps to root `/` URL
- 
 import variables from './variables.module.scss'
- 
+import styles from '@/app/georgia.module.scss'
+import Image from 'next/image'
 export default function Page() {
-  return <h1 style={{ color: variables.primaryColor }}>Hello, Next.js!</h1>
+  return (
+// header
+<header className={styles.header}>
+  <div className={styles.brand}>
+    <a href='#'><i className='fas fa-utensils'></i></a>
+    <div>
+      <h1 className={styles.mainName}>Georgia</h1>
+      <p className={styles.subName}>Restaurant</p>
+    </div>
+    <div className={styles.banner}>
+      <h1 className={styles.mainHeading}>Welcome</h1>
+      <h3 className={styles.subHeading}>Try great Georgian dishes</h3>
+      <button className={styles.mainBtn} type='button'>Reservation</button>
+    </div>
+  </div>
+</header>
+  )
 }
