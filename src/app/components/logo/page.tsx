33e31ff2e9a314480c8052666c691409a53d4e0c
aeropriest <ashok.jaiswal@gmail.com>
2024-08-09 08:40:39 +0800

    * Added the logo

---
+++ src/app/components/logo/page.tsx
@@ -0,0 +1,6 @@
+import { Stint_Ultra_Condensed } from 'next/font/google';
+import styles from './styles.module.scss';
+
+export default function Logo() {
+    return(<div><p className={styles.logo}>This is logo</p></div>)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import variables from './variables.module.scss'
 import styles from '@/app/georgia.module.scss'
 import Image from 'next/image'
+import Logo from './components/logo/page'
 export default function Page() {
   return (
 // header
 <header className={styles.header}>
   <div className={styles.brand}>
+    <Logo/>
     <a href='#'><i className='fas fa-utensils'></i></a>
     <div>
       <h1 className={styles.mainName}>Georgia</h1>
