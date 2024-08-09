805aacb5cf43fe40fbd226acf5b1d07d37ff3a61
aeropriest <ashok.jaiswal@gmail.com>
2024-08-09 20:28:06 +0800

    * Removed the georgia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import Header from "./components/header/page";
-import AboutUs from "./components/about";
-import Gallery from "./components/gallery";
-import Footer from "./components/footer";
 export default function Page() {
   return(
     <div>
-      <Header />
-      <AboutUs />
-      {/* <Gallery/> */}
-      <Footer />
+        This is the main
     </div>
   )
 }
