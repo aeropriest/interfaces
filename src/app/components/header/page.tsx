cfb669bf30072e4c4256f903d51c70afc56c6911
aeropriest <ashok.jaiswal@gmail.com>
2024-08-09 13:01:20 +0800

    * Refactoring the component folder

---
+++ src/app/components/header/page.tsx
@@ -0,0 +1,38 @@
+import styles from "@/app/georgia.module.scss";
+import Logo from '@/app/components/logo'
+import Button from "@/app/components/button";
+
+import { josefinSans, nunito, dancingScript } from "@/app/fonts";
+
+export default function Header() {
+  return (
+    // header
+    <header className={styles.header}>
+      <div className={styles.brand}>
+        <Logo />
+        <a href="#">
+          <i className="fas fa-utensils"></i>
+        </a>
+        <div>
+          <div className={styles.mainName}>
+            <p className={nunito.className}>Georgia</p>
+          </div>
+          <div className={styles.subName}>
+            <p className={josefinSans.className}>Restaurant</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.banner}>
+          <div className={styles.mainHeading}>
+            <p className={dancingScript.className}>
+              Welcome
+            </p>
+          </div>
+          <div className={styles.subHeading}>
+            <p className={josefinSans.className}>Try great Georgian dishes</p>
+          </div>
+            <Button></Button>
+      </div>      
+    </header>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,7 @@
-import styles from "@/app/georgia.module.scss";
-import Logo from "./components/logo/page";
-import Button from "./components/button/page";
-
-import { josefinSans, nunito, dancingScript } from "@/app/fonts";
+import Header from "./components/header/page";
 
 export default function Page() {
-  return (
-    // header
-    <header className={styles.header}>
-      <div className={styles.brand}>
-        <Logo />
-        <a href="#">
-          <i className="fas fa-utensils"></i>
-        </a>
-        <div>
-          <div className={styles.mainName}>
-            <p className={nunito.className}>Georgia</p>
-          </div>
-          <div className={styles.subName}>
-            <p className={josefinSans.className}>Restaurant</p>
-          </div>
-        </div>
-      </div>
-      <div className={styles.banner}>
-          <div className={styles.mainHeading}>
-            <p className={dancingScript.className}>
-              Welcome
-            </p>
-          </div>
-          <div className={styles.subHeading}>
-            <p className={josefinSans.className}>Try great Georgian dishes</p>
-          </div>
-            <Button></Button>
-      </div>      
-    </header>
-  );
+  return(
+    <Header />
+  )
 }
