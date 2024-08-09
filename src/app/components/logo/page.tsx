bba354217220d650a9be5db767ca109d4a374ac9
aeropriest <ashok.jaiswal@gmail.com>
2024-08-09 09:21:44 +0800

    * Added the fonts and text styles

--- src/app/components/logo/page.tsx
+++ src/app/components/logo/page.tsx
@@ -1,6 +1,10 @@
 import { Stint_Ultra_Condensed } from 'next/font/google';
 import styles from './styles.module.scss';
+import { FaUtensils } from 'react-icons/fa'; // Importing the fork and spoon icon
+
 
 export default function Logo() {
-    return(<div><p className={styles.logo}>This is logo</p></div>)
+    return(<div className={styles.border}>              
+        <FaUtensils className={styles.logo} />
+    </div>)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,13 @@ import variables from './variables.module.scss'
 import styles from '@/app/georgia.module.scss'
 import Image from 'next/image'
 import Logo from './components/logo/page'
+
+import { Dancing_Script, Josefin_Sans, Nunito } from 'next/font/google';
+const dancingScript = Dancing_Script({ subsets: ['latin'], variable: '--dancing-script-font' });
+const josefinSans = Josefin_Sans({ subsets: ['latin'], variable: '--josefin-sans-font' });
+const nunito = Nunito({ subsets: ['latin'], variable: '--nunito-font' });
+
+
 export default function Page() {
   return (
 // header
@@ -10,14 +17,17 @@ export default function Page() {
     <Logo/>
     <a href='#'><i className='fas fa-utensils'></i></a>
     <div>
-      <h1 className={styles.mainName}>Georgia</h1>
+    
+      <div className={styles.mainName}>
+        <h1 className={nunito.className}>Georgia</h1>
+      </div>
       <p className={styles.subName}>Restaurant</p>
     </div>
-    <div className={styles.banner}>
+    {/* <div className={styles.banner}>
       <h1 className={styles.mainHeading}>Welcome</h1>
       <h3 className={styles.subHeading}>Try great Georgian dishes</h3>
       <button className={styles.mainBtn} type='button'>Reservation</button>
-    </div>
+    </div> */}
   </div>
 </header>
   )
