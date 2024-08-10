d43e243b0bd159c6aa7fb5b8a51057bd23fed81a
aeropriest <ashok.jaiswal@gmail.com>
2024-08-10 09:27:34 +0800

    Added assets

---
+++ src/app/components/logo/page.tsx
@@ -0,0 +1,6 @@
+import styles from './styles.module.scss'
+import { josefinSans, nunito, dancingScript } from "@/app/fonts";
+
+export default function Logo(){
+    return(<div className={styles.text}><p className={dancingScript.className}>Jaismina</p></div>)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Logo from './components/logo/page'
 import styles from './styles.module.scss'
 export default function Page() {
   return(
     <div className={styles.main}>
-        <h1 className={styles.text}>This is the main</h1>
+        {/* <h1 className={styles.text}>This is the main</h1> */}
+        <Logo/>
     </div>
   )
 }
